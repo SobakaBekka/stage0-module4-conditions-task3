9744567b4bd5d7c2a57b69dd0b2778b7be57739c
SobakaBekka <st69704@upce.cz>
2024-02-20 21:59:53 +0100

    fixed the bug n the SalaryCalculator

--- src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
+++ src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
@@ -4,13 +4,13 @@ public class SalaryCalculator {
     public void calculateSalary(int salary) {
         double salary_1 = (double)salary;
         if(salary_1 <= 10000 && salary_1 > 0){
-            salary_1 = salary_1 - ((salary_1 / 100) * 10);
-            System.out.println(salary_1);
-        } else if (salary_1 >= 10000 && salary_1 <= 20000) {
             salary_1 = salary_1 - ((salary_1 / 100) * 15);
             System.out.println(salary_1);
-        } else if (salary_1 > 20000) {
+        } else if (salary_1 >= 10000 && salary_1 <= 20000) {
             salary_1 = salary_1 - ((salary_1 / 100) * 18);
+            System.out.println(salary_1);
+        } else if (salary_1 > 20000) {
+            salary_1 = salary_1 - ((salary_1 / 100) * 20);
             System.out.println(salary_1);
         }else {
             System.out.println("wrong input!");
